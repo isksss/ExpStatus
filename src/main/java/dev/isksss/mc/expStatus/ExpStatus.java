abe9a6643be049285a4eab4d26a48b602a5323f1
isksss <104404522+isksss@users.noreply.github.com>
2024-10-03 19:13:17 +0900

    init

---
+++ src/main/java/dev/isksss/mc/expStatus/ExpStatus.java
@@ -0,0 +1,17 @@
+package dev.isksss.mc.expStatus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ExpStatus extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
